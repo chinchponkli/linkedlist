e3256224c2f01ffca6ef1dbf32e15253825fa9af
chinchponkli <arjit.agarwal.1000@gmail.com>
2019-07-24 13:40:27 +0530

    Check palindrome stack based

---
+++ checkPalindrome.py
@@ -0,0 +1,39 @@
+'''
+Given a singly linked list of characters, write a function that returns true
+if the given list is palindrome, else false.
+The list below is palindrome:
+{Empty List}
+1
+1 -> 2 -> 2 -> 1
+'''
+
+from linkedlist import Node
+from linkedlist import LinkedList
+
+'''
+Approach 1 (Using stack):
+1) Traverse the given list from head to tail and push every visited node to stack.
+2) Traverse the list again. For every visited node, pop a node from stack and
+compare data of popped node with currently visited node.
+3) If all nodes matched, then return true, else false.
+Time complexity of above method is O(n), but it requires O(n) extra space.
+'''
+
+def checkPalindromeStackBased(node):
+    s = []
+    current = node
+    while current is not None:
+        s.append(current.data)
+        current = current.next
+    current = node
+    while current is not None:
+        if current.data != s.pop():
+            return False
+        s.append(current.data)
+        current = current.next
+    return True
+
+l = LinkedList()
+l.append(1)
+l.append(2)
+print checkPalindromeStackBased(l.head)
