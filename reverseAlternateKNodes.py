c66477863fbf5804b77781c25ce409705db1e356
chinchponkli <arjit.agarwal.1000@gmail.com>
2019-07-25 14:10:45 +0530

    Reverse alternate k nodes

---
+++ reverseAlternateKNodes.py
@@ -0,0 +1,34 @@
+'''
+Given a linked list, write a function to reverse every alternate k nodes in an
+efficient way.
+Example:
+Inputs:   1->2->3->4->5->6->7->8->9->NULL and k = 3
+Output:   3->2->1->4->5->6->9->8->7->NULL.
+'''
+from linkedlist import LinkedList
+from linkedlist import Node
+
+def reverseAlternateKNodes(node, k, reverse):
+    prev, current, count, next = None, node, 0, None
+    while current is not None and count < k:
+        if reverse:
+            next = current.next
+            current.next = prev
+            prev, current = current, next
+            count += 1
+        else:
+            prev, current, next = current, current.next, current.next
+            count += 1
+    if reverse:
+        if next is not None:
+            node.next = reverseAlternateKNodes(next, k, not reverse)
+    else:
+        if prev is not None:
+            prev.next = reverseAlternateKNodes(next, k, not reverse)
+    return prev if reverse else node
+
+l = LinkedList()
+[l.append(x) for x in range(1, 10)]
+l.printList()
+l.head = reverseAlternateKNodes(l.head, 3, True)
+l.printList()
