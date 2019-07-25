37e9cc1fd5dcdc87e2b8560ff9941daecfe505d5
chinchponkli <arjit.agarwal.1000@gmail.com>
2019-07-25 13:55:44 +0530

    Stack based solution for reversing linked list in group of size k

--- reverseListInGroupOfGivenSize.py
+++ reverseListInGroupOfGivenSize.py
@@ -12,7 +12,7 @@ from linkedlist import LinkedList
 
 def reverse(node, k):
     prev, current, count = None, node, 0
-    while current and count < k:
+    while current is not None and count < k:
         next = current.next
         current.next = prev
         prev, current = current, next
@@ -22,12 +22,40 @@ def reverse(node, k):
     Else it points to (k+1)th node. Intital node's next will point to the reverse
     of the next segment of the list. Simple.
     '''
-    if next:
+    if next is not None:
         node.next = reverse(next, k)
-    return prev
+    return
+
+'''
+Firstly, push the k elements of the linked list in the stack.
+Now pop elements one by one and keep track of the previously popped node.
+Point the next pointer of prev node to top element of stack.
+Repeat this process, until NULL is reached.
+
+This algorithm uses O(k) extra space.
+'''
+def reverseStackBased(l, k):
+    s = []
+    prev, current = None, l.head
+    while current is not None:
+        count = 0
+        while current is not None and count < k:
+            s.append(current)
+            current = current.next
+            count += 1
+        while len(s) > 0:
+            if prev is not None:
+                prev.next = s[len(s) - 1]
+                prev = prev.next
+                s.pop()
+            else:
+                prev = s[len(s) - 1]
+                l.head = prev
+                s.pop()
+    prev.next = None
 
 l = LinkedList()
-[l.append(x) for x in range(1, 9)]
+[l.append(x) for x in range(1, 10)]
 l.printList()
-l.head = reverse(l.head, 3)
+reverseStackBased(l, 3)
 l.printList()
