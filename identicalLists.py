e5c28d004d0ccb7444499dba89bdec7fda41b9b5
chinchponkli <arjit.agarwal.1000@gmail.com>
2019-07-25 18:25:30 +0530

    Check is lists are identical

---
+++ identicalLists.py
@@ -0,0 +1,20 @@
+'''
+Two Linked Lists are identical when they have same data and arrangement of data
+is also same. For example Linked lists a (1->2->3) and b(1->2->3) are identical.
+Write a function to check if the given two linked lists are identical.
+'''
+from linkedlist import LinkedList
+from linkedlist import Node
+
+def areIdentical(a, b):
+    if a is not None and b is not None:
+        return a.data == b.data and areIdentical(a.next, b.next)
+    elif a is not None or b is not None:
+        return False
+    else:
+        return True
+
+l = LinkedList()
+[l.append(x) for x in range(1, 8)]
+
+print areIdentical(l.head, l.head)
